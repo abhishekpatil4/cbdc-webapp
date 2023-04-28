5d43eb945538b930aea80bafd6512c81e6384d9d
Abhishek Patil <abishkpatil@gmail.com>
2023-04-28 10:51:26 +0530

    added transaction history page

---
+++ pages/transactions.js
@@ -0,0 +1,9 @@
+export default function transactions(){
+    return(
+        <>
+        <div className="pl-96 pt-24 font-black text-8xl">
+            Work in progress..
+        </div>
+        </>
+    )
+}
